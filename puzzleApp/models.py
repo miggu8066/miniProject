d4b617da5f7f4e6e41db8a7b9d8dcb5365394fc7
EGChan <egc1005@naver.com>
2023-01-09 11:55:32 +0900

    ㄱ1

--- puzzleApp/models.py
+++ puzzleApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Puzzle(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     score = models.IntegerField()
     date = models.CharField(max_length=255)
 
